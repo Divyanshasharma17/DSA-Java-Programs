82e3774baac3c85318c8192ac00ebdf4e5c3a466
Divyanshasharma17 <divyanshasharma02@gmail.com>
2025-08-24 21:36:59 +0530

    Update TwoSumArray.java

--- Arrays/TwoSumArray.java
+++ Arrays/TwoSumArray.java
@@ -28,8 +28,9 @@ Space Complexity: O(1)
 ===========================================================
 */
 
-public class TwoSum {
+Solution:-
 
+public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
